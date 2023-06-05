d9ed3f5e4df5a550f3c2bd6c3857273b16316fb9
Guardian <guardian.mech0502@gmail.com>
2023-06-05 13:39:14 +0200

    Initial commit

---
+++ Test/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Ciao, inserisci un numero, il numero verra stampato" << std::endl;
+    int n;
+    std::cin >> n;
+    std::cout << "Il numero inserito è: " << n << std::endl;
+    return 0;
+}
